89eca0f7c13abb0edd70c3b25449e4da9933f00d
LittleBigPants <juanmanuel_garcia98@hotmail.com>
2023-12-10 17:01:55 -0300

    cors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const routerApi = require('./routes');
 const {logErrors,  errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
@@ -6,6 +7,17 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+const whitelist = ['http://localhost:3000', 'http://myapp.co', 'http://localhost:8080'];
+const options = {
+  origin: (origin, cb) => {
+    if (whitelist.includes(origin) || !origin) {
+      cb(null, true);
+    } else {
+      cb(new Error ('no permitido'));
+    }
+  }
+}
+
 
 app.get('/', (req, res) => {
   res.send("muy buenas chavales");
@@ -16,7 +28,7 @@ app.listen(port, () => {
 });
 
 routerApi(app);
-
+app.use(cors(options));
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
