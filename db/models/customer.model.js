075734a87a8d035a0cb8bccad302f04333dd35c0
LittleBigPants <juanmanuel_garcia98@hotmail.com>
2023-12-17 20:48:31 -0300

    customer-model-dev

---
+++ db/models/customer.model.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const CUSTOMER_TABLE = 'customers';
+
+const CustomerSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'create_at',
+    defaultValue: Sequelize.NOW
+  },
+  role: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    defaultValue: 'customer'
+  }
+}
+
+class Customer extends Model {
+  static associate() {
+    // associate
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: CUSTOMER_TABLE,
+      modelName: 'Customer',
+      timestamps: false
+    }
+  }
+}
