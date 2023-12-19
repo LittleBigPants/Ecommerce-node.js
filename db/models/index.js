022ed00001afbc30f3c57466915702486277b1b9
LittleBigPants <juanmanuel_garcia98@hotmail.com>
2023-12-19 12:23:50 -0300

    order-model

--- db/models/index.js
+++ db/models/index.js
@@ -3,17 +3,20 @@ const { User, UserSchema } = require('./user.model');
 const {Product, ProductSchema} = require('./product.model');
 const {Category, CategorySchema } = require('./category.model');
 const { Customer, CustomerSchema } = require('./customer.model');
+const { Order, OrderSchema } = require('./order.model');
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Customer.init(CustomerSchema, Customer.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
+  Order.init(OrderSchema, Order.config(sequelize));
 
   Product.associate(sequelize.models);
   Category.associate(sequelize.models);
   User.associate(sequelize.models);
   Customer.associate(sequelize.models);
+  Order.associate(sequelize.models);
 
 }
 
